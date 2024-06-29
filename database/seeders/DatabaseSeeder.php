276aacfc7141140d1e3c7d91d4c355e561c9a20a
Victor Valdovinos <victor_mvo1@hotmail.com>
2024-06-29 12:07:31 -0600

    Configuracion de login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Victor Valdovinos',
+        //     'email' => 'victor@example.com',
+        //     'password' => Hash::make('password'),
+        // ]);
+
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
